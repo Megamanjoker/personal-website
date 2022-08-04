910a0190accbacc748c64eda7d98589c4c7cf94e
alexyikeh <alex.ikeh@bimodalconsulting.com>
2022-08-04 11:00:01 +0100

    fix: mongo error and set up code

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://lyft_user:lyft_pass1@ds353338.mlab.com:53338/lyft',
+    mongoURI: 'mongodb+srv://codedfingers:Kingkuntab430@cluster0.vmbae.mongodb.net/test',
     testMongoURI: 'mongodb://test-db-user:test-db-pass1@ds141641.mlab.com:41641/test-db'
 }
