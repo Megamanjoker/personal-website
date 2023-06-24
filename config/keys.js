e254cdc457eb2bd2e3364fb9d91fb2768e43020b
Tyler <megamanjoker@live.com>
2023-06-23 23:33:14 -0500

    cleaning up files

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://codedfingers:Kingkuntab430@cluster0.vmbae.mongodb.net/test',
-    testMongoURI: 'mongodb://test-db-user:test-db-pass1@ds141641.mlab.com:41641/test-db'
-}
